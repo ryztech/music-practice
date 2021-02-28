0fb84f79d60aa9bf34a0a4e03a84fcf8819cfc14
Ryz Jenvey <rjenvey@hotmail.co.uk>
2021-02-28 18:44:41 +0000

    start adding example songs to progressions

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 const courseContent = {
     patterns:
-        {
-            handy: [
-                'Straight beat gospel style',
-                'Half beat bounce',
-                'Split chord (and 7ths)',
-                'Bossa Nova Bounce'
-            ],
-            other: [
-                'left note / right chord',
-                'oom pah',
-                '12/8 country shuffle',
-                'Twist',
-                'Twist-2',
-                '3 beat bounce'
-            ],
-            blues: [
-                'straight eight boogie (alternating)',
-                'straight eight boogie (constant)',
-                'blueberry hill',
-                '12/8 boogie shuffle',
-                'blues brothers'
-            ]
-        },
+    {
+        handy: [
+            'Straight beat gospel style',
+            'Half beat bounce',
+            'Split chord (and 7ths)',
+            'Bossa Nova Bounce'
+        ],
+        other: [
+            'left note / right chord',
+            'oom pah',
+            '12/8 country shuffle',
+            'Twist',
+            'Twist-2',
+            '3 beat bounce'
+        ],
+        blues: [
+            'straight eight boogie (alternating)',
+            'straight eight boogie (constant)',
+            'blueberry hill',
+            '12/8 boogie shuffle',
+            'blues brothers'
+        ]
+    },
     balladStyle: {
         pieces: [
             '\'mixing it all together\'',
@@ -37,11 +37,12 @@ const courseContent = {
 
 const keys = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
 const progressions = [
-    'I-IV-V',
-    'I – V – vi – IV',
-    'ii – V – I',
-    'I – V – vi – iii – IV – I – IV – V',
-    'I - vi - ii - V'
+    { numerals: 'I - IV - V', exampleSongs: ['Good Riddance'] },
+    { numerals: 'I – V – vi – IV', exampleSongs: ['Let It Be', 'Torn'] },
+    { numerals: 'ii – V – I', exampleSongs: ['Sunday Morning'] },
+    { numerals: 'I – V – vi – iii – IV – I – IV – V', exampleSongs: ['Firework', 'Under The Bridge'] },
+    { numerals: 'I - vi - ii - V', exampleSongs: ['foo'] },
+    { numerals: 'I - vi - IV - V', exampleSongs: ['Stand By Me', 'Every Breath You Take', 'Perfect'] },
 ];
 const inversionStartingPoints = ['root', 'middle', 'backwards']
 const forwardsBackwards = ['forwards', 'backwards']
@@ -56,12 +57,12 @@ let fullSessionText = ''
 const line = (text) => fullSessionText += (fullSessionText === '' ? (`${text}`) : (`\n\n${text}`))
 const r = (list) => list[Math.floor(Math.random() * list.length)]
 const rs = (list) => list.splice(Math.floor(Math.random() * list.length), 1)
-const lineRandomly = (oneInX, text) => { if (Math.floor(Math.random() * oneInX) + 1 === oneInX ) line(`${text}`)}
+const lineRandomly = (oneInX, text) => { if (Math.floor(Math.random() * oneInX) + 1 === oneInX) line(`${text}`) }
 
 line('#####################')
 lineRandomly(5, 'Song: Broken Chord Ballad')
-line(`Pattern style: ${r(courseContent.patterns.handy)}, in ${rs(keys)}, with progression ${rs(progressions)}`)
-line(`Pattern style: ${r(courseContent.patterns.other)}, in ${rs(keys)}, with progression ${rs(progressions)}`)
+line(`Pattern style: ${r(courseContent.patterns.handy)}, in ${rs(keys)}, with progression ${rs(progressions)[0].numerals}`)
+line(`Pattern style: ${r(courseContent.patterns.other)}, in ${rs(keys)}, with progression ${rs(progressions)[0].numerals}`)
 line(`Blues: ${r(courseContent.patterns.blues)}, in ${rs(keys)}`)
 line(`Inversions: starting with ${r(inversionStartingPoints)}, ${r(forwardsBackwards)}`)
 lineRandomly(3, 'Theory flashcards')
